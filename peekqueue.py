9634775552d3858c92e858a9b822e07ee57d4e49
Horror10314 <chapsh0525@gmail.com>
2025-09-20 02:20:03 +1000

    modify peekqueue

--- peekqueue.py
+++ peekqueue.py
@@ -1,6 +1,32 @@
-from queue import Queue
+from queue import PriorityQueue
 
-class PeekableQueue(Queue):
+class PeekableQueue(PriorityQueue):
+    # priority queue is minheap
+    # which mean the fist (top) item of priority queue is the smallest
+    # thsu the last element should be the biggest
+
+    def _init(self, maxsize):
+        super()._init(maxsize)
+        self.last = None
+
+    def _put(self, item):
+        super()._put(item)
+        if self.last is None:
+            self.last = item
+        else:
+            self.last = item if self.last < item else self.last # new bigger item is added, update last
+    
+    def _get(self):
+        ret = super()._get()
+        if not self.queue:  # no element in queue => no last
+            self.last = None
+        return ret
+
+    def decrease(self, sub):
+        "decrease the all priority values in the queue by sub"
+        for item in self.queue:
+            item -= sub
+
     def peek_first(self):
         with self.mutex:
             if self.queue:
@@ -9,6 +35,5 @@ class PeekableQueue(Queue):
     
     def peek_last(self):
         with self.mutex:
-            if self.queue:
-                return self.queue[-1]
-        return None
+            return self.last
+
